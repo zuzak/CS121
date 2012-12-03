6f58f718c0d42602ed37299eaa477ac54ebd94ac
Douglas Gardner <douglas@chippy.ch>
2012-12-03 16:01:01 +0000

    add border to board
    Board now supports padding to the side
    Board now has a border
    
    Need to find a better corner character.

--- Board.java
+++ Board.java
@@ -10,7 +10,6 @@ public class Board {
 	}
 
 	public void setPoint(int x, int y, char charToSet) {
-		System.out.println("DEBUG: setting point (" + x + "," + y + ")");
 		theBoard[x][y] = charToSet;
 	}
 
@@ -20,14 +19,32 @@ public class Board {
 
 	public void print() {
 		String currentRow = new String();
+		printPadding(Constants.PADDING);
+		System.out.print(Constants.CORNER);
 		for (int i = 0; i < Constants.WIDTH; i++) {
+			System.out.print(Constants.BORDERX);	
+		}
+		System.out.print(Constants.CORNER+"\n");
+		for (int i = 0; i < Constants.WIDTH; i++) {
+			printPadding(Constants.PADDING);
+			currentRow = "" + Constants.BORDERY;
 			for (int j = 0; j < Constants.HEIGHT; j++) {
-				//currentRow.concat(getPoint(i,j));
 				currentRow = currentRow + getPoint(i,j);
-				//System.out.println(getPoint(i,j)+".");
 			}
+			currentRow = currentRow + Constants.BORDERY;
 			System.out.println(currentRow);
-			currentRow = "";
+		}
+		printPadding(Constants.PADDING);
+		System.out.print(Constants.CORNER);
+		for (int i = 0; i < Constants.WIDTH; i++) {
+			System.out.print(Constants.BORDERX);
+		}
+		System.out.print(Constants.CORNER+"\n");
+	}
+
+	public void printPadding(int padding) {
+		for (int i = 0; i < padding; i++) {
+			System.out.print(Constants.PADDINGCHAR);
 		}
 	}
 }
--- Constants.java
+++ Constants.java
@@ -1,5 +1,13 @@
 interface Constants { // not sure what an interface is, but hey
 	public final static char ZERO = '@';
+	public final static char PLAYER = 'P';
+	public final static char BLOCK = 'B';
+	public final static char SHADOW = 'S';
+	public final static char BORDERY = '|';
+	public final static char CORNER = '+';
+	public static final char BORDERX = '―';
 	public final static int WIDTH = 12;
 	public final static int HEIGHT = 12;
+	public final static int PADDING = 3;
+	public final static char PADDINGCHAR = ' ';
 }
