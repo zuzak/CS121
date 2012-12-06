89c54b477d177175d726ccc1178daeefb1f79eda
Douglas Gardner <douglas@chippy.ch>
2012-12-06 17:24:20 +0000

    lol broken code, let's commit it anyway

---
+++ RunGame.java
@@ -0,0 +1,13 @@
+/*
+ *
+ * DOUGLAS GARDNER         DOG2
+ * ABERYSTWYTH UNIVERSITY CS121
+ * 
+ */
+public class RunGame {
+//	Game game = new Game();
+	public static void main(String args[]){
+		Game game = new Game();
+		game.Begin();
+	}
+}
